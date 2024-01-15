8758226653b90ebd8b867e8c551729bf83767572
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-01-15 11:41:28 +0545

    added esewa flutter sdk

---
+++ lib/core/constants/esewa_credential.dart
@@ -0,0 +1,5 @@
+class EsewaCredential{
+  static const clientId =
+    "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R";
+  static const secretId = "BhwIWQQADhIYSxILExMcAgFXFhcOBwAKBgAXEQ==";
+}
