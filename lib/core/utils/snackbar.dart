9f3f5ca14099187accbf3774284a86834c5d2d51
Bigyan Thanait <bigyanthanait8@gmail.com>
2024-01-23 18:40:14 +0545

    change design of snackbar

--- lib/core/utils/snackbar.dart
+++ lib/core/utils/snackbar.dart
@@ -3,5 +3,8 @@ import 'package:flutter/material.dart';
 void showSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context)
     ..hideCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text(text)));
+    ..showSnackBar(SnackBar(
+      backgroundColor: Theme.of(context).primaryColor,
+      content: Text(text),
+    ));
 }
